4bd756043418686959f8e6bd8fc4d9e2d53addbe
Deeter <deeter.cesler@gmail.com>
2018-12-19 22:26:41 -0500

    scrolling test

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 
 
 class PageTwo extends React.Component {
@@ -29,10 +29,14 @@ export default class App extends React.Component {
       return;
     }
     return (
+      <ScrollView>
         <View style={styles.container}>
           <Text>Open up App.js to start working on your app!</Text>
           <Text>This is an edit. Bitch.</Text>
           <Text>Number of times pressed: {this.state.amount}</Text>
+          <Text>This is an edit. Bitch.</Text>
+          <Text>This is an edit. Bitch.</Text>
+          <Text>This is an edit. Bitch.</Text>
           {/* <Button title="baby button" onPress={console.log("BABY")}>This is a button.</Button> */}
           {/* <View > */}
             {/* <Text style={styles.containerTwo}>Next page</Text> */}
@@ -44,6 +48,7 @@ export default class App extends React.Component {
             accessibilityLabel="Learn more about this purple button"
           />
         </View>
+        </ScrollView>
     );
   }
 }
@@ -51,6 +56,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 2000,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
